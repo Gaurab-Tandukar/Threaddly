4552555b284171fb502995e1e01708d208e03629
Gaurab Tandukar <gaurabtandukar20@gmail.com>
2026-06-01 13:25:41 +0545

    Initialize new Project with Spring Boot

---
+++ src/main/java/com/Threaddly/Threaddly/test.java
@@ -0,0 +1,14 @@
+package com.Threaddly.Threaddly;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    
+    // @RequestMapping("test")
+    // public String test() {
+    //     return "<h1>Hello, World!</h1>";
+    // }
+
+   
+}
---
+++ src/test/java/com/Threaddly/Threaddly/ThreaddlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Threaddly.Threaddly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreaddlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
